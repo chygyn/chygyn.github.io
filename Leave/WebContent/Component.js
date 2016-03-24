7ced8091eca541eeac31058c0c9e8d0339820759
chygyn <pavlovegor.msk@gmail.com>
2016-03-24 17:20:53 +0300

    ыва

--- Leave/WebContent/Component.js
+++ Leave/WebContent/Component.js
@@ -1,5 +1,3 @@
-//библиотека плагинов
-//jQuery.sap.declare("sap.ui.demo.myFiori.Component");
 
 sap.ui.core.UIComponent.extend("Leave.Component", {
   getController: function () {
@@ -7,26 +5,18 @@ sap.ui.core.UIComponent.extend("Leave.Component", {
   },
   
   createContent : function() {
-    // скрол бар
-//    this.setDisplayBlock(false);
-    // корневая вьюха
+
     var oView = sap.ui.view({
       id : "app",
       viewName : "Leave.view.App",
       type : "XML",
       viewData : { component : this }
     });
-    // создаем сплит апп
-    // скрол барkjkjk
-
 
     this.app = oView.byId("idApp");
     var oModel = new sap.ui.model.json.JSONModel("model/requests.json");
     oView.setModel(oModel,"requests");
-    // мастерs
-    //var master = sap.ui.xmlview("Master", "sap.ui.demo.myFiori.view.Master");
-
-    
+   
     return oView;
   } 
 });
